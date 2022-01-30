9eaa1ee5cbbaa567925cf347384ecf12d0e06a2c
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-30 15:04:55 -0600

    Create a variable to keep track of current card, store DOM cards, store cardData in an array with as many card objects as needed. Hard code 3 to begin with

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+
+// Keep track of current card
+let currentActiveCard = 0;
+
+// Store DOM cards
+const cardsEl = [];
+
+// Store card data
+const cardsData = [
+	{
+		question: 'What must a variable begin with?',
+		answer: 'A letter, $ or _'
+	},
+	{
+		question: 'What is a variable?',
+		answer: 'A container for a piece of data'
+	},
+	{
+		question: 'Example of Case Sensitive Variable',
+		answer: 'thisIsAVariable'
+	}
+]
