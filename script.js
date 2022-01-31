a6a2ef997ad5a206b114b4a676fd87dccacd6027
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-30 19:43:25 -0600

    Add event listener to clear cards button to remove all cards from local storage

--- script.js
+++ script.js
@@ -151,3 +151,11 @@ addCardBtn.addEventListener('click', ()=> {
 	}
 
 });
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+	localStorage.clear();
+	cardsContainer.innerHTML = '';
+	window.location.reload();
+
+});
